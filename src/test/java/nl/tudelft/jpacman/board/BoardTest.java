44db5764e9e04896e3c54d331e657e7d9bdf24f5
JuiPi <sorawit2800@gmail.com>
2025-11-05 16:11:38 +0700

    Implement BoardTest.java

---
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -0,0 +1,45 @@
+package nl.tudelft.jpacman.board;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class BoardTest {
+    private Board board;
+    private BasicSquare square;
+
+    @BeforeEach
+    public void setup() {
+        square = new BasicSquare();
+        Square[][] grid = new Square[][] { { square } };
+        board = new Board(grid);
+    }
+
+    @Test
+    void testBoardSizeOne() {
+        // Board should not be null
+        assertThat(board).isNotNull();
+
+        // Board should return the same square we created
+        assertThat(board.squareAt(0, 0)).isEqualTo(square);
+
+        // The square should not be null
+        assertThat(board.squareAt(0, 0)).isNotNull();
+
+        // The width and height should both  be 1
+        assertThat(board.getWidth()).isEqualTo(1);
+        assertThat(board.getHeight()).isEqualTo(1);
+    }
+
+    @Test
+    void testBoardWithNullSquare() {
+        // Create a board grid with a null square
+        Square[][] invalidGrid = new Square[][] { { null } };
+
+        assertThrows(AssertionError.class, () -> {
+            new Board(invalidGrid);
+        });
+    }
+}
