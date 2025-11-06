c9471214aa73cd0a4eec67a01dc69ada629ccee9
JuiPi <sorawit2800@gmail.com>
2025-11-07 00:32:21 +0700

    Implement testParseMapWrong() in MapParserTest.java

--- src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
+++ src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -1,7 +1,9 @@
 package nl.tudelft.jpacman.level;
 
+import nl.tudelft.jpacman.PacmanConfigurationException;
 import nl.tudelft.jpacman.board.BoardFactory;
 import nl.tudelft.jpacman.npc.ghost.Blinky;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -56,4 +58,25 @@ public class MapParserTest {
         Mockito.verify(levelFactory, Mockito.times(expectedPellets)).createPellet();
     }
 
+
+    /**
+     * Test for parseMap with invalid input (wrong map format).
+     */
+    @Test
+    public void testParseMapWrong() {
+        PacmanConfigurationException thrown =
+            Assertions.assertThrows(PacmanConfigurationException.class, () -> {
+                MockitoAnnotations.initMocks(this);
+                assertNotNull(boardFactory);
+                assertNotNull(levelFactory);
+                MapParser mapParser = new MapParser(levelFactory, boardFactory);
+                ArrayList<String> map = new ArrayList<>();
+                map.add("#####");
+                map.add("#P G");
+                map.add("######");
+                mapParser.parseMap(map);
+            });
+
+        Assertions.assertEquals("Input text lines are not of equal width.", thrown.getMessage());
+    }
 }
